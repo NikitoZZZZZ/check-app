3e7096db81b6b6462003481454100eeacb12dd8c
NikitoZZZZZ <manakovnikita95@ya.ru>
2017-10-29 15:09:32 +0300

    add GET request method

--- server/src/main/java/com/netcracker/checkapp/server/controller/CheckController.java
+++ server/src/main/java/com/netcracker/checkapp/server/controller/CheckController.java
@@ -6,10 +6,12 @@ import com.netcracker.checkapp.server.persistance.CheckRepository;
 import com.netcracker.checkapp.server.service.checkservice.CheckService;
 import com.netcracker.checkapp.server.service.checkservice.CheckServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -30,6 +32,26 @@ public class CheckController {
         responseEntity = new ResponseEntity<Check>(checkService.getCheck(params.get("fdocumentn"), params.get("fdriven"),
                 params.get("fs")), HttpStatus.OK);
         checkRepository.save(responseEntity.getBody());
+
+        return responseEntity;
+    }
+
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<List<Check>> getAll() {
+        ResponseEntity<List<Check>> responseEntity;
+
+        responseEntity = new ResponseEntity<List<Check>>(checkRepository.findAll(), HttpStatus.OK);
+
+        return responseEntity;
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Check> getById(@PathVariable String id) {
+        ResponseEntity<Check> responseEntity;
+
+        responseEntity = new ResponseEntity<Check>(checkRepository.findById(id), HttpStatus.OK);
 
         return responseEntity;
     }
