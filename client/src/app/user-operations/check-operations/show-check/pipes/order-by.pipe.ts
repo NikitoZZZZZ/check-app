8d3d654812e49a236859c6982cacdec965d9b649
Ваше имя <tangord@mail.ru>
2017-11-15 19:40:25 +0300

    delete some functions in 'order-by pipe' issue-9-ui

--- client/src/app/user-operations/check-operations/show-check/pipes/order-by.pipe.ts
+++ client/src/app/user-operations/check-operations/show-check/pipes/order-by.pipe.ts
@@ -16,12 +16,6 @@ export class OrderByPipe implements PipeTransform {
     return a > b ? 1 : -1;
   }
 
-  static parseExpression(expression: string): string[] {
-    expression = expression.replace(/\[(\w+)\]/g, '.$1');
-    expression = expression.replace(/^\./, '');
-    return expression.split('.');
-  }
-
   static getValue(object: any, expression: string[]) {
     for (let i = 0, n = expression.length; i < n; ++i) {
       const k = expression[i];
@@ -34,15 +28,6 @@ export class OrderByPipe implements PipeTransform {
     return object;
   }
 
-  static setValue(object: any, value: any, expression: string[]) {
-    let i;
-    for (i = 0; i < expression.length - 1; i++) {
-      object = object[expression[i]];
-    }
-
-    object[expression[i]] = value;
-  }
-
   transform(value: any | any[], expression?: any, reverse?: boolean, isCaseInsensitive: boolean = false): any {
     if (!value) {
       return value;
@@ -51,19 +36,11 @@ export class OrderByPipe implements PipeTransform {
     if (Array.isArray(value)) {
       return this.sortArray(value, expression, reverse, isCaseInsensitive);
     }
-
-    if (typeof value === 'object') {
-      return this.transformObject(value, expression, reverse, isCaseInsensitive);
-    }
-
     return value;
   }
 
   private sortArray(value: any[], expression?: any, reverse?: boolean, isCaseInsensitive?: boolean): any[] {
     const isDeepLink = expression && expression.indexOf('.') !== -1;
-    if (isDeepLink) {
-      expression = OrderByPipe.parseExpression(expression);
-    }
 
     let array: any[] = value.sort((a: any, b: any): number => {
       if (!expression) {
@@ -101,23 +78,4 @@ export class OrderByPipe implements PipeTransform {
     return array;
   }
 
-  private transformObject(value: any | any[], expression?: any, reverse?: boolean, isCaseInsensitive?: boolean): any {
-
-    let parsedExpression = OrderByPipe.parseExpression(expression);
-    let lastPredicate = parsedExpression.pop();
-    let oldValue = OrderByPipe.getValue(value, parsedExpression);
-
-    if (!Array.isArray(oldValue)) {
-      parsedExpression.push(lastPredicate);
-      lastPredicate = null;
-      oldValue = OrderByPipe.getValue(value, parsedExpression);
-    }
-
-    if (!oldValue) {
-      return value;
-    }
-
-    OrderByPipe.setValue(value, this.transform(oldValue, lastPredicate, reverse, isCaseInsensitive), parsedExpression);
-    return value;
-  }
 }
