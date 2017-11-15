1f7893e5c47899f331c12955ea78b8912371d1bf
Igor Konovalov <rogee.nok@gmail.com>
2017-11-15 22:10:23 +0300

    add FiscalDriveNumber & ShortPlace link POJO

---
+++ server/src/main/java/com/netcracker/checkapp/server/model/FDSP.java
@@ -0,0 +1,29 @@
+package com.netcracker.checkapp.server.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Fiscal Drive Number and Short Place document class
+ */
+@Document(collection = "fdsp")
+public class FDSP {
+
+    private String fiscalDriveNumber;
+    private ShortPlace shortPlace;
+
+    public String getFiscalDriveNumber() {
+        return fiscalDriveNumber;
+    }
+
+    public void setFiscalDriveNumber(String fiscalDriveNumber) {
+        this.fiscalDriveNumber = fiscalDriveNumber;
+    }
+
+    public ShortPlace getShortPlace() {
+        return shortPlace;
+    }
+
+    public void setShortPlace(ShortPlace shortPlace) {
+        this.shortPlace = shortPlace;
+    }
+}
