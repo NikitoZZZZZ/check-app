2f7bd1b334fa697863847bfe44dc98e2db17c3c7
Igor Konovalov <rogee.nok@gmail.com>
2017-11-15 14:29:33 +0300

    change base service name to HttpService

--- server/src/main/java/com/netcracker/checkapp/server/service/HttpService.java
+++ server/src/main/java/com/netcracker/checkapp/server/service/HttpService.java
@@ -6,6 +6,6 @@ import org.springframework.http.MediaType;
 import java.util.List;
 import java.util.Map;
 
-public interface Service {
+public interface HttpService {
     public HttpHeaders addHeaders(Map<String, String> map);
 }
