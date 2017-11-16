a1006134700499fed9b426a9f9c6120b37d9de85
Igor Konovalov <rogee.nok@gmail.com>
2017-11-16 21:21:05 +0300

    expand Place Service

--- server/src/main/java/com/netcracker/checkapp/server/service/placeservice/PlaceService.java
+++ server/src/main/java/com/netcracker/checkapp/server/service/placeservice/PlaceService.java
@@ -1,7 +1,14 @@
 package com.netcracker.checkapp.server.service.placeservice;
 
 import com.netcracker.checkapp.server.model.Place;
+import com.sun.prism.image.Coords;
+
+import java.util.List;
 
 public interface PlaceService {
     public Place incrementPlaceRating(Place place);
+
+    public Place addNewPlace(Place place);
+
+    public List<Place> getNearPlaces(Coords coords, double radius);
 }
--- server/src/main/java/com/netcracker/checkapp/server/service/placeservice/PlaceServiceImpl.java
+++ server/src/main/java/com/netcracker/checkapp/server/service/placeservice/PlaceServiceImpl.java
@@ -2,7 +2,10 @@ package com.netcracker.checkapp.server.service.placeservice;
 
 import com.netcracker.checkapp.server.model.Place;
 import com.netcracker.checkapp.server.persistance.PlaceRepository;
+import com.sun.prism.image.Coords;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PlaceServiceImpl implements PlaceService {
@@ -14,6 +17,24 @@ public class PlaceServiceImpl implements PlaceService {
 
     @Override
     public Place incrementPlaceRating(Place place) {
-        return placeRepository.findCustomOneAndUpdate(place.getId());
+        if (placeRepository.updatePlace(place.getId()) != 0)
+            return placeRepository.findOne(place.getId());
+        else
+            return null;
+    }
+
+    @Override
+    public Place addNewPlace(Place place) {
+        Place localPlace = placeRepository.findOne(place.getId());
+        if (localPlace != null)
+            localPlace = incrementPlaceRating(place);
+        else
+            localPlace = placeRepository.save(place);
+        return localPlace;
+    }
+
+    @Override
+    public List<Place> getNearPlaces(Coords coords, double radius) {
+        return null;
     }
 }
