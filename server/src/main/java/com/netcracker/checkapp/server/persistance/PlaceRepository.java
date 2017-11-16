06628911e1880da994a361cc521412e79c692071
Igor Konovalov <rogee.nok@gmail.com>
2017-11-16 21:18:58 +0300

    add custom methods to Place Repository

--- server/src/main/java/com/netcracker/checkapp/server/persistance/PlaceRepository.java
+++ server/src/main/java/com/netcracker/checkapp/server/persistance/PlaceRepository.java
@@ -1,15 +1,12 @@
 package com.netcracker.checkapp.server.persistance;
 
 import com.netcracker.checkapp.server.model.Place;
-import com.sun.prism.image.Coords;
+import com.netcracker.checkapp.server.persistance.customs.PlaceRepositoryCustom;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlaceRepository extends MongoRepository<Place, String> {
+public interface PlaceRepository extends MongoRepository<Place, String>, PlaceRepositoryCustom {
 
-    Place findByCoords(Coords coords);
-
-    // method for incrementing place numOfChecks - should be implemented by ourselves
-    Place findCustomOneAndUpdate(String id);
+    // other methods, if necessary
 }
