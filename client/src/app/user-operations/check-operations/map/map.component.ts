c55db76496c8cb467d8aed51e9179ea4990e9cf3
Ваше имя <tangord@mail.ru>
2017-11-18 16:57:35 +0300

    add coord marker  issue-15-ui

--- client/src/app/user-operations/check-operations/map/map.component.ts
+++ client/src/app/user-operations/check-operations/map/map.component.ts
@@ -9,9 +9,17 @@ export class MapComponent implements OnInit {
 
   lat: number = 59.929428;
   lng: number = 30.362017;
+
+  markerlat: number = 0;
+  markerlng: number = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setMarkerEvent(lat: number,lng: number){
+    this.markerlat=lat;
+    this.markerlng=lng;
   }
 
 }
