a633ab52e83067709ef6db3886a0691d7d1fb0d9
NikitoZZZZZ <manakovnikita95@ya.ru>
2017-11-28 19:26:57 +0300

    change registration form (not finished, temporary)

--- client/src/app/login/login/login.component.ts
+++ client/src/app/login/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   fullUser: FullUser = new FullUser();
   done = false;
   loginUrl = '/login';
-  registerUrl = '/register';
+  registerUrl = '/register'
 
   constructor(private httpService: HttpService,
               private router: Router,
