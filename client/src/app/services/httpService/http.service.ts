6436ec78be132be07b095314ef038ded0973a743
NikitoZZZZZ <manakovnikita95@ya.ru>
2017-11-29 00:36:19 +0300

    fix (pull request #17)

--- client/src/app/services/httpService/http.service.ts
+++ client/src/app/services/httpService/http.service.ts
@@ -16,7 +16,9 @@ export class HttpService {
   postData(params: string, url: string) {
     const headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
     return this.http.post(url, params, {headers: headers})
-      // .map(res => res.json())
+      .map((res: Response) => {
+      return res;
+      })
       .catch((error: any) => Observable.throw(error));
   }
 
@@ -27,7 +29,9 @@ export class HttpService {
   postBody(params: string, url: string) {
     let headers = new Headers({'Content-Type': 'application/json'});
     return this.http.post(url, params, {headers: headers})
-      // .map(res => res.json())
+      .map((res: Response) => {
+        return res;
+      })
       .catch((error: any) => Observable.throw(error));
   }
 
